e0671e5c3bbbad73f0e35e4be42733b9b9a978c3
Yves Faems <yves.faems@gmail.com>
2022-03-01 18:53:31 -0800

    update urls

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -14,7 +14,7 @@ const projectInfo = [
         key: 1,
         title: "Patient Care Report GENERATOR",
         description: "EMTs have to write a report for every patient transport they handle. This app generates such a detailed and carefully written report by simply adjusting recurring parameters and asking for case-by-case specific input.",
-        mainTechnologies: "JavaScript, Bootstrap",
+        mainTechnologies: "HTML, Bootstrap, JavaScript",
         picUrl: imagePCRGEN,
         netlifyUrl: "https://naughty-rosalind-082c27.netlify.app/",
         githubCodeAddress : "github code url"
@@ -23,9 +23,9 @@ const projectInfo = [
         key: 2,
         title: "Dictionary App",
         description: "Latin to Dutch Vocabulary App",
-        mainTechnologies: "CSS, JavaScript",
+        mainTechnologies: "HTML, CSS, JavaScript",
         picUrl: imageItinera,
-        netlifyUrl: "https://www.youtube.com/watch?v=Vg6K4pn_C8o",
+        netlifyUrl: "https://gallant-perlman-46617b.netlify.app/",
         githubCodeAddress : "https://github.com/Qasper1974/sev"
     },
     {
@@ -34,7 +34,7 @@ const projectInfo = [
         description: "A Swahili to English Vocabulary App",
         mainTechnologies: "Django, PostgreSQL, Docker, AWS",
         picUrl: imageSev,
-        netlifyUrl: "https://portfolio-yfaems.netlify.app",
+        netlifyUrl: "https://www.youtube.com/watch?v=Vg6K4pn_C8o",
         githubCodeAddress : "github code url"
     },
     {
